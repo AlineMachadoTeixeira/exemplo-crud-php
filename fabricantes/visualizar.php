fdc0c6c9dadbca1350a4c2bed60b3880e28cae6c
Aline Teixeira <alinemachadoteixeira@gmail.com>
2023-08-24 11:51:03 -0300

    Fazer a saída de dados com uma tabela

--- fabricantes/visualizar.php
+++ fabricantes/visualizar.php
@@ -10,6 +10,39 @@ $listaDeFabricantes = lerfabricantes($conexao);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Fabricantes - Visualização</title>
+
+    <style>
+        
+
+        table, td, tr {
+             border: 1px solid; 
+             border-collapse: collapse;  
+             margin: auto; 
+             background-color: cornsilk;          
+        }      
+        
+        caption{          
+              font-size: 30px;
+              padding: 5px;              
+        }
+
+        th{
+            background-color: #000000;
+            color: #fff;
+            padding: 10px;            
+        }
+
+        td{ 
+            padding: 10px 50px;               
+        }      
+
+        tr:hover{
+            background-color: bisque;
+            color: #000000;
+        }
+    </style>
+
+
 </head>
 <body>
     <h1>Fabricantes | SELECT -  
@@ -22,6 +55,29 @@ $listaDeFabricantes = lerfabricantes($conexao);
     <p>
         <a href="inserir.php">Inserir novo fabricante</a>
     </p>
+
+    <!-- Fazer a saída de dados com uma tabela -->
+    <table>
+        <caption>Lista de Fabricantes</caption>
+        <tr>
+            <th>ID</th>
+            <th>Nome</th>
+            <th colspan="2">Operação</th>
+        </tr>
+
+   <?php 
+   foreach ($listaDeFabricantes  as $listaDeFabricante ){       
+   ?>  
+            
+        <tr>
+            <td><?=$listaDeFabricante["id"]?></td>
+            <td><?=$listaDeFabricante["nome"]?></td>
+            <td><a href="">Editar</a>  <a href="">Excluir</a></td>                
+        </tr>      
+   <?php       
+     }
+   ?>
+    </table>
     
 </body>
 </html>
