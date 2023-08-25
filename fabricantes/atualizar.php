34a7645102a077c79d1df499767ac4c4cef2db14
Aline Teixeira <alinemachadoteixeira@gmail.com>
2023-08-25 11:22:12 -0300

    Carregamentos dos dados fabricantes

--- fabricantes/atualizar.php
+++ fabricantes/atualizar.php
@@ -1,8 +1,15 @@
 <?php
+/* Importando as funções de fabricantes */
+require_once "../src/funcoes-fabricantes.php";
+
 /* Obtendo e sanitizando o valor vindo do parâmetro de URL (link dinâmico) */
 $id = filter_input(INPUT_GET, "id", FILTER_SANITIZE_NUMBER_INT);
 
+/* Chamando a função e recuperando os dados de um fabricante de acordo com id passado */
+$fabricante = lerUmFabricante($conexao, $id);
 ?>
+
+
 
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -17,9 +24,11 @@ $id = filter_input(INPUT_GET, "id", FILTER_SANITIZE_NUMBER_INT);
     <hr>
 
     <form action="" method="post">
+        <!-- Campos  aculto usado apenas registro no formulário do id do fabricante que está sendo visualizado. -->
+        <input type="hidden" name="id" value="<?=$fabricante['id']?>">
         <p>
             <label for="nome">Nome:</label>
-            <input required type="text" name="nome" id="nome">
+            <input value="<?=$fabricante['nome']?>" required type="text" name="nome" id="nome">
         </p>
         <button type="submit" name="atualizar">Atualizar Fabricante</button>
 
